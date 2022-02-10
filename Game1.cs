f5582039bda1ce2f2b8dcb82d1d6ae80a8ac9ac5
Starkj13 <kjellqvist13@gmail.com>
2022-02-10 14:05:03 +0100

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Space_Invators
+{
+    public class Game1 : Game
+    {
+        GameState _state;
+        Texture2D P1Pic, BulletPic;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public static int Width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        public static int Height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+        // ints
+        int MovmentSpeed = 8;
+
+
+        //Rectangle
+        Rectangle RectP1 = new Rectangle(Width/2, Height /2 + 500, 80, 80);
+
+        // Vector
+        Vector2 BulletPosition1;
+
+
+
+        enum GameState
+        {
+            MainMenu,
+            GamePlay,
+        }
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = Width;
+            _graphics.PreferredBackBufferHeight = Height;
+            _graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            P1Pic = Content.Load<Texture2D>("parrot");
+            BulletPic = Content.Load<Texture2D>("ball");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+             _state = GameState.GamePlay;
+
+            switch (_state)
+            {
+                case GameState.MainMenu:
+                    break;
+                case GameState.GamePlay:
+                    GamePlay(gameTime);
+                    break;
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            base.Draw(gameTime);
+        }
+
+        void GamePlay(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Blue);
+            KeyboardState KeyboardState = Keyboard.GetState();
+            BulletPosition1.X = Width / 2 - (18 / 2);
+            BulletPosition1.Y = Height / 2 - (18 / 2);
+
+            _spriteBatch.Begin();
+
+            // Player Draw
+            _spriteBatch.Draw(P1Pic, RectP1, Color.White);
+            _spriteBatch.Draw(BulletPic, BulletPosition1, Color.White);
+
+            _spriteBatch.End();
+
+            MovementPlayer();
+
+
+            base.Draw(gameTime);
+        }
+
+
+        void MovementPlayer()
+        {
+            KeyboardState KeyboardState = Keyboard.GetState();
+
+            if (KeyboardState.IsKeyDown(Keys.A))
+            {
+                RectP1.X -= MovmentSpeed;
+            }
+            if (KeyboardState.IsKeyDown(Keys.D))
+            {
+                RectP1.X += MovmentSpeed;
+            }
+
+
+            if (RectP1.X > Width - 85)
+            {
+                RectP1.X -= MovmentSpeed;
+            }
+            if (RectP1.X < 0)
+            {
+                RectP1.X += MovmentSpeed;
+            }
+
+        }
+
+    }
+}
