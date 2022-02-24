4466ba4de2f8c97eebf535a366fc4f30d1100774
Starkj13 <kjellqvist13@gmail.com>
2022-02-24 14:13:34 +0100

    space

--- Game1.cs
+++ Game1.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace Space_Invators
 {
     public class Game1 : Game
     {
-        GameState _state;
-        Texture2D P1Pic, BulletPic;
+        Random Random = new Random();
+
+        GameState _state; 
+        MouseState mouse;
+        Texture2D P1Pic, BulletPic, EnemyPic;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -15,17 +23,26 @@ namespace Space_Invators
         public static int Width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
         public static int Height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
 
+        bool Hit = false;
+
+
         // ints
         int MovmentSpeed = 8;
+        int test = 0;
 
+        List<Rectangle> rects = new List<Rectangle>();
+        
 
         //Rectangle
         Rectangle RectP1 = new Rectangle(Width/2, Height /2 + 500, 80, 80);
+        Rectangle EnemyRect1 = new Rectangle(0, Height/5, 80, 80);
+        Rectangle BulletRect;
+
+
 
         // Vector
-        Vector2 BulletPosition1;
-
-
+        Vector2 BulletSpeed;
+        Vector2 BulletPosition;
 
         enum GameState
         {
@@ -47,7 +64,7 @@ namespace Space_Invators
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            
             base.Initialize();
         }
 
@@ -57,11 +74,13 @@ namespace Space_Invators
 
             P1Pic = Content.Load<Texture2D>("parrot");
             BulletPic = Content.Load<Texture2D>("ball");
+            EnemyPic = Content.Load<Texture2D>("LargeAlien");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -92,23 +111,23 @@ namespace Space_Invators
         {
             GraphicsDevice.Clear(Color.Blue);
             KeyboardState KeyboardState = Keyboard.GetState();
-            BulletPosition1.X = Width / 2 - (18 / 2);
-            BulletPosition1.Y = Height / 2 - (18 / 2);
+            mouse = Mouse.GetState();
 
             _spriteBatch.Begin();
 
             // Player Draw
             _spriteBatch.Draw(P1Pic, RectP1, Color.White);
-            _spriteBatch.Draw(BulletPic, BulletPosition1, Color.White);
-
+            _spriteBatch.Draw(BulletPic, BulletPosition, Color.White);
+            _spriteBatch.Draw(EnemyPic, EnemyRect1, Color.White);
             _spriteBatch.End();
+
+            BulletPosition += BulletSpeed;
 
             MovementPlayer();
 
 
             base.Draw(gameTime);
         }
-
 
         void MovementPlayer()
         {
@@ -133,7 +152,32 @@ namespace Space_Invators
                 RectP1.X += MovmentSpeed;
             }
 
+
+            if (mouse.LeftButton == ButtonState.Pressed && Hit == false)
+            {
+                BulletPosition.X = RectP1.X;
+                BulletPosition.Y = RectP1.Y;
+
+                BulletSpeed.Y = Random.Next(-10, -5);
+                Hit = true;
+            }
+           
+            if (BulletPosition.Y < -Height/2)
+            { 
+                Hit = false;
+            }
+
         }
 
-    }
+        void Collicon()
+        {
+            BulletRect.Y = (int)BulletPosition.Y;
+            BulletRect.X = (int)BulletPosition.X;
+
+            if (BulletRect.Intersects(EnemyRect1) == true);
+            {
+                test++;
+            }
+        }
+    }   
 }
